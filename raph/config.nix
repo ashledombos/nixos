efc6b720398c0a1936fe07801de8fd35cbc768d4
ashledombos <rj@hodo.fr>
2024-06-05 00:47:08 +0200

    Update config.nix

--- raph/config.nix
+++ raph/config.nix
@@ -1,110 +1,94 @@
 { config, pkgs, ... }:
 
+let
+  # Configuration du dépôt Git local
+  #=====================================================================================
+  gitRepo = "/etc/nixos/git/nixos";
+
+  # Script de mise à jour Git avant chaque rebuild
+  gitPull = pkgs.writeShellScript "git-pull.sh" ''
+    cd "${gitRepo}"
+    git fetch --dry-run 2>&1 | grep -q -v 'up to date' && git pull || echo "No changes to pull."
+  '';
+in
 {
+  # Paquets Système de Base
+  #=====================================================================================
   environment.systemPackages = with pkgs; [
     git
     kdePackages.discover
     adi1090x-plymouth-themes
   ];
-  # Configuration de SDDM
-  services.displayManager.sddm = {
-    # autoLogin.enable = false;
-    # theme = "breeze";  # Thème par défaut, ajustez selon besoins
-    settings = {
-      General = {
-        # Définir l'environnement de greeter pour utiliser le français
-        GreeterEnvironment = "LANG=fr_FR.UTF-8";
-        # Activer le pavé numérique
-        Numlock = "on";
-      };
+
+  # Configuration de l'Écran de Connexion SDDM
+  #=====================================================================================
+  services.displayManager.sddm.settings = {
+    General = {
+      GreeterEnvironment = "LANG=fr_FR.UTF-8"; # Langue du greeter
+      Numlock = "on"; # Activation du pavé numérique
     };
   };
 
-
-  # Paramètres pour zram avec systemd
-  #services.zram-generator.enable = true;
-  #services.zram-generator.settings = {
-  #  zram0 = {
-  #    host-memory-limit = "none";  # Pas de limite spécifique à la mémoire hôte
-  #    zram-fraction = 0.5;  # Utiliser 50% de la mémoire RAM disponible pour zram
-  #    max-zram-size = "8192";  # Taille maximale de zram à 8192 MiB
-  #    compression-algorithm = "lzo-rle";  # Algorithme de compression recommandé
-  #  };
-  #};
-
-  # Power management
-  #powerManagement.enable = true;
-  #boot.kernelModules = [ "zram" ];
-  #boot.initrd.availableKernelModules = [ "zram" ];
-  #boot.initrd.kernelModules = [ "zram" ];
-  #boot.resumeDevice = "/dev/disk/by-uuid/bf81d449-7430-425a-b3f9-46e8de342731";
-  #boot.kernelParams = [
-  #  "resume=UUID=bf81d449-7430-425a-b3f9-46e8de342731"
-  #  "resume_offset=64223232"
-  #];
-  #systemd.sleep.extraConfig = ''
-  #  HibernateDelaySec=30s # very low value to test suspend-then-hibernate
-  # SuspendState=mem #standby freeze # suspend2idle is buggy :(
-  #'';
-
-  # Enable Discover and Flatpak
+  # Support des Applications et Mises à Jour
+  #=====================================================================================
+  # Activation de fwupd pour la mise à jour du firmware
   services.fwupd.enable = true;
+  
+  # Support de Flatpak et PackageKit pour la gestion des applications
   services.flatpak.enable = true;
   services.packagekit.enable = true;
-  xdg.portal.extraPortals = 
-    [ pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-kde
-    ];
-  # xdg.portal.config.common.default = "kde";
+  
+  # Portails supplémentaires pour le bureau KDE
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-kde ];
 
-  # Active les mises à jour automatiques avec persistance
+  # Gestion Automatique des Mises à Jour
+  #=====================================================================================
   system.autoUpgrade = {
     enable = true;
-    dates = "Sun *-*-* 21:30:00";  # Chaque samedi à 02:00 AM
+    dates = "Sun *-*-* 21:30:00"; # Mise à jour automatique chaque dimanche à 21:30
   };
 
-  systemd.timers.auto-upgrade = {
-    timerConfig.Persistent = true;  # Assure que les tâches manquées sont rattrapées
-  };
+  systemd.timers.auto-upgrade.timerConfig.Persistent = true;
 
-  # Active le nettoyage automatique du store Nix
+  # Maintenance Automatique de Nix Store
+  #=====================================================================================
   systemd.services.nix-gc-optimize = {
     description = "Nix Garbage Collect and Store Optimization";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeScript "nix-maintenance" ''
-        #!/bin/sh
-        ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 30d
-        ${pkgs.nix}/bin/nix-store --optimize
-      '';
-      User = "root";
-      Group = "root";
-    };
+    serviceConfig.Type = "oneshot";
+    serviceConfig.ExecStart = pkgs.writeScript "nix-maintenance" ''
+      #!/bin/sh
+      ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 30d
+      ${pkgs.nix}/bin/nix-store --optimize
+    '';
     wantedBy = [ "multi-user.target" ];
   };
 
   systemd.timers.nix-gc-optimize = {
     description = "Weekly timer for Nix garbage collection and optimization";
-    wantedBy = [ "timers.target" ];
     timerConfig = {
       OnCalendar = "Sun 21:30:00";
       Persistent = true;
     };
+    wantedBy = [ "timers.target" ];
   };
 
-  # Activer Plymouth
+  # Configuration de Plymouth pour l'Écran de Démarrage
+  #=====================================================================================
   boot.plymouth = {
     enable = true;
-    theme = "rings_2";
-    themePackages = [ pkgs.adi1090x-plymouth-themes ];
+    theme = "rings";
+    themePackages = with pkgs; [
+      (adi1090x-plymouth-themes.override {
+        selected_themes = [ "rings" ];
+      })
+    ];
   };
-  #boot.initrd.kernelModules = [ "fbcon" "vesa_fbcon" ];
-  #boot.initrd.systemd.enable = true;
-  #boot.initrd.systemd.services.plymouth-start.enable = true;
-  #boot.initrd.systemd.services.plymouth-start.wantedBy = [ "initrd.target" ];
-  boot = {
-    kernelParams = [ "quiet" "splash" ];
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-  };
+
+  # Configuration Générale de Nix
+  #=====================================================================================
+  nix.extraOptions = ''
+    keep-derivations = true
+    keep-outputs = true
+  '';
+  system.extraDerivedProductHooks = [ gitPull ];
 }
