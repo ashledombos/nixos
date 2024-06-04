093525386981c6a91e67dba4bf6e864872333398
ashledombos <rj@hodo.fr>
2024-06-05 01:04:21 +0200

    Update services.nix

--- raph/services.nix
+++ raph/services.nix
@@ -1,55 +1,65 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 let
   gitRepo = "/etc/nixos/git/nixos";
-  gitPullScript = pkgs.writeShellScriptBin "git-pull" ''
-    cd "${gitRepo}"
-    git fetch --dry-run 2>&1 | grep -q -v 'up to date' && git pull || echo "No changes to pull."
-  '';
 in
 {
   systemd.services.git-pull = {
     description = "Git repository auto-update";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = gitPullScript;
+      ExecStart = "${pkgs.writeShellScriptBin "git-pull" ''
+        #!/bin/sh
+        cd "${gitRepo}"
+        git fetch --dry-run 2>&1 | grep -q -v 'up to date' && git pull || echo "No changes to pull."
+      ''}";
     };
-    wantedBy = [ "multi-user.target" ];
   };
 
   systemd.timers.git-pull = {
     description = "Periodically pull git repository";
-    partOf = [ "git-pull.service" ];
     wantedBy = [ "timers.target" ];
-    timerConfig.OnCalendar = "hourly";
+    timerConfig = {
+      OnCalendar = "hourly";
+      Persistent = true;
+    };
   };
 
   systemd.services.nix-gc-optimize = {
     description = "Nix Garbage Collect and Store Optimization";
-    serviceConfig.Type = "oneshot";
-    serviceConfig.ExecStart = "${pkgs.writeScript "nix-maintenance" ''
-      ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 30d
-      ${pkgs.nix}/bin/nix-store --optimize
-    ''}";
     wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.writeScriptBin "nix-maintenance" ''
+        #!/bin/sh
+        ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 30d
+        ${pkgs.nix}/bin/nix-store --optimize
+      ''}";
+    };
   };
 
   systemd.timers.nix-gc-optimize = {
     description = "Weekly timer for Nix garbage collection and optimization";
+    wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "Sun 21:30:00";
+      OnCalendar = "weekly";
       Persistent = true;
     };
-    wantedBy = [ "timers.target" ];
   };
 
   system.autoUpgrade = {
     enable = true;
-    dates = "Sun *-*-* 21:30:00"; # Mise à jour automatique chaque dimanche à 21:30
+    allowReboot = true;
   };
 
   systemd.timers.auto-upgrade = {
-    timerConfig.Persistent = true;
+    description = "Automatically upgrade NixOS system";
     wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "Sun *-*-* 21:30:00";
+      Persistent = true;
+    };
   };
 }
