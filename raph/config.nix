26bf83ca4bf6d6ccfcfae0ed2b5e09db545a9fce
ashledombos <rj@hodo.fr>
2024-06-05 00:56:35 +0200

    Update config.nix

--- raph/config.nix
+++ raph/config.nix
@@ -1,19 +1,12 @@
 { config, pkgs, ... }:
 
-let
-  # Configuration du dépôt Git local
-  #=====================================================================================
-  gitRepo = "/etc/nixos/git/nixos";
-
-  # Script de mise à jour Git avant chaque rebuild
-  gitPull = pkgs.writeShellScript "git-pull.sh" ''
-    cd "${gitRepo}"
-    git fetch --dry-run 2>&1 | grep -q -v 'up to date' && git pull || echo "No changes to pull."
-  '';
-in
 {
+
+  imports = [
+    ./services.nix
+  ];
+
   # Paquets Système de Base
-  #=====================================================================================
   environment.systemPackages = with pkgs; [
     git
     kdePackages.discover
@@ -21,7 +14,6 @@ in
   ];
 
   # Configuration de l'Écran de Connexion SDDM
-  #=====================================================================================
   services.displayManager.sddm.settings = {
     General = {
       GreeterEnvironment = "LANG=fr_FR.UTF-8"; # Langue du greeter
@@ -30,50 +22,12 @@ in
   };
 
   # Support des Applications et Mises à Jour
-  #=====================================================================================
-  # Activation de fwupd pour la mise à jour du firmware
   services.fwupd.enable = true;
-  
-  # Support de Flatpak et PackageKit pour la gestion des applications
   services.flatpak.enable = true;
   services.packagekit.enable = true;
-  
-  # Portails supplémentaires pour le bureau KDE
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-kde ];
 
-  # Gestion Automatique des Mises à Jour
-  #=====================================================================================
-  system.autoUpgrade = {
-    enable = true;
-    dates = "Sun *-*-* 21:30:00"; # Mise à jour automatique chaque dimanche à 21:30
-  };
-
-  systemd.timers.auto-upgrade.timerConfig.Persistent = true;
-
-  # Maintenance Automatique de Nix Store
-  #=====================================================================================
-  systemd.services.nix-gc-optimize = {
-    description = "Nix Garbage Collect and Store Optimization";
-    serviceConfig.Type = "oneshot";
-    serviceConfig.ExecStart = pkgs.writeScript "nix-maintenance" ''
-      #!/bin/sh
-      ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 30d
-      ${pkgs.nix}/bin/nix-store --optimize
-    '';
-    wantedBy = [ "multi-user.target" ];
-  };
-
-  systemd.timers.nix-gc-optimize = {
-    description = "Weekly timer for Nix garbage collection and optimization";
-    timerConfig = {
-      OnCalendar = "Sun 21:30:00";
-      Persistent = true;
-    };
-    wantedBy = [ "timers.target" ];
-  };
-
   # Configuration de Plymouth pour l'Écran de Démarrage
-  #=====================================================================================
   boot.plymouth = {
     enable = true;
     theme = "rings";
@@ -83,12 +37,4 @@ in
       })
     ];
   };
-
-  # Configuration Générale de Nix
-  #=====================================================================================
-  nix.extraOptions = ''
-    keep-derivations = true
-    keep-outputs = true
-  '';
-  system.extraDerivedProductHooks = [ gitPull ];
 }
