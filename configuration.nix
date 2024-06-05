7cb8f6879880502d102765ab5767050a226e0984
ashledombos <rj@hodo.fr>
2024-06-05 23:45:39 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./raph/config.nix
+      ./raph/machines/artichaut.nix
     ];
 
   # Bootloader.
