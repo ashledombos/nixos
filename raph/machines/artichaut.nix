bba142e0105b468aace347eb6ac82dedd5facc86
ashledombos <rj@hodo.fr>
2024-06-06 00:05:41 +0200

    Update artichaut.nix

--- raph/machines/artichaut.nix
+++ raph/machines/artichaut.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }:
 
 {
-
   imports = [
     ../config.nix
     ../services.nix
     ../boot-plymouth.nix
     # ../portable.nix
   ];
+  boot.initrd.availableKernelModules = [ "i915" ];
+  boot.kernelModules = [ "i915" ];
+  boot.earlyModuleLoads = [ "i915" ];
 }
