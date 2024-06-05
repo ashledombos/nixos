fea9ef860da1ca32f0a42efdab3f010a872b174a
ashledombos <rj@hodo.fr>
2024-06-06 00:33:52 +0200

    Update boot-plymouth.nix

--- raph/boot-plymouth.nix
+++ raph/boot-plymouth.nix
@@ -4,12 +4,12 @@
 
 {
   # Masquer les messages de démarrage et n'afficher que l'écran Plymouth
-  # boot.kernelParams = [ "quiet" ];
+  boot.kernelParams = [ "quiet" ];
 
   # Désactiver l'éditeur de menu systemd-boot pour masquer le menu de sélection des générations
   # boot.loader.systemd-boot.editor = false;
-  boot.initrd.kernelModules = [ "intel_agp" ];
-  boot.kernelParams = [ "vga=795" "splash=verbose" ];
+  # boot.initrd.kernelModules = [ "intel_agp" ];
+  # boot.kernelParams = [ "vga=795" "splash=verbose" ];
 
   # Activer Plymouth avec le thème "rings"
   #boot.plymouth = {
@@ -20,7 +20,7 @@
   boot.plymouth = {
     enable = true;
     # debug = true;
-    theme = "fade-in";
+    theme = "spinner";
     # themePackages = with pkgs; [
     #  (adi1090x-plymouth-themes.override {
     #    selected_themes = [ "rings" ];
