6c786d354e54f6d26c4df0298e5050c39fbce79e
ashledombos <rj@hodo.fr>
2024-06-05 23:44:48 +0200

    Update config.nix

--- raph/config.nix
+++ raph/config.nix
@@ -28,13 +28,13 @@
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-kde ];
 
   # Configuration de Plymouth pour l'Écran de Démarrage
-  boot.plymouth = {
-    enable = true;
-    theme = "rings";
-    themePackages = with pkgs; [
-      (adi1090x-plymouth-themes.override {
-        selected_themes = [ "rings" ];
-      })
-    ];
-  };
+  # boot.plymouth = {
+  # enable = true;
+  #  theme = "rings";
+  #  themePackages = with pkgs; [
+  #    (adi1090x-plymouth-themes.override {
+  #      selected_themes = [ "rings" ];
+  #    })
+  #  ];
+  #};
 }
