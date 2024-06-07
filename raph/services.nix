f9a21072046e40dcf2191e754b07fa614c651ad4
ashledombos <rj@hodo.fr>
2024-06-08 00:41:00 +0200

    Update services.nix

--- raph/services.nix
+++ raph/services.nix
@@ -5,7 +5,7 @@
   system.autoUpgrade = {
     enable = true;
     allowReboot = true;
-    dates = "03:00:00"; 
+    #dates = "03:00:00"; 
     #fixedRandomDelay = true;
     #operation = "boot";
   };
