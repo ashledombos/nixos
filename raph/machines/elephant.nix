2750a9b1a65f209160b528135327f170ff93a52d
ashledombos <rj@hodo.fr>
2024-06-07 23:54:12 +0200

    Update elephant.nix

--- raph/machines/elephant.nix
+++ raph/machines/elephant.nix
@@ -7,4 +7,22 @@
     ../boot-plymouth.nix
     ../portable.nix
   ];
+
+  # Paramètres du noyau nécessaires pour l'hibernation
+  boot.kernelParams = [
+    "resume=UUID=<UUID du swap>"
+    "resume_offset=<Offset du swap>"
+  ];
+
+  # Configuration spécifique à GRUB pour l'hibernation
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    extraConfig = "resume=UUID=<UUID du swap> resume_offset=<Offset du swap>";
+  };
+
+  # Assurez-vous que ces valeurs sont correctement remplies avant de reconstruire
+  # Utilisez les commandes suivantes pour obtenir ces informations:
+  # UUID: findmnt -no UUID -T /home/swapfile
+  # Offset: sudo filefrag -e /home/swapfile
 }
