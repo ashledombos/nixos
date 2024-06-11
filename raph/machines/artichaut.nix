02b95b35d609aecfbb077eac637c8ac55da69cef
ashledombos <rj@hodo.fr>
2024-06-11 19:28:20 +0200

    Update artichaut.nix

--- raph/machines/artichaut.nix
+++ raph/machines/artichaut.nix
@@ -6,5 +6,13 @@
     ../services.nix
     ../boot-plymouth.nix
     # ../portable.nix
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2; 
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev"; # Ou périph de démarrage
+
+
   ];
 }
