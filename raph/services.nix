834f61695efc1479a7ef9562d9e6cf55a1dbbf60
ashledombos <rj@hodo.fr>
2024-06-15 14:43:55 +0200

    Update services.nix

--- raph/services.nix
+++ raph/services.nix
@@ -1,18 +1,19 @@
 { config, pkgs, ... }:
 
 {
-# mise à jour
+  imports = [];
+
+  # Mise à jour automatique du système
   system.autoUpgrade = {
     enable = true;
     allowReboot = true;
-    #dates = "03:00:00"; 
-    #fixedRandomDelay = true;
-    #operation = "boot";
+    dates = "Mon *-*-* 03:00:00"; # Exécute les mises à jour chaque lundi à 3h00
+    channel = "https://nixos.org/channels/nixos-24.05";
   };
 
-# Active le nettoyage automatique du store Nix
+  # Configuration du service de nettoyage et d'optimisation du store Nix
   systemd.services.nix-gc-optimize = {
-    description = "Nix Garbage Collect and Store Optimization";
+    description = "Nettoyage du Garbage Collect Nix et optimisation du store";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
@@ -26,61 +27,40 @@
     };
   };
 
+  # Timer pour le nettoyage automatique
   systemd.timers.nix-gc-optimize = {
-    description = "Weekly timer for Nix garbage collection and optimization";
+    description = "Timer hebdomadaire pour le nettoyage et l'optimisation du store Nix";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "Sun 21:30:00";
-      Persistent = true;
+      OnCalendar = "Mon *-*-* 04:00:00";  # Lancement chaque lundi après les mises à jour
+      Persistent = true;  # S'exécute après un redémarrage si l'heure prévue est manquée
     };
   };
 
+  # Configuration du service de mise à jour automatique du dépôt Git
   systemd.services.git-pull = {
-    description = "Git repository auto-update";
+    description = "Mise à jour automatique du dépôt Git";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
       ExecStart = pkgs.writeScript "git-pull" ''
         #!/bin/sh
-        #export PATH=${pkgs.git}/bin:$PATH
         cd "/etc/nixos/git/nixos"
-        ${pkgs.git}/bin/git fetch --dry-run 2>&1 | grep -q -v 'up to date' && ${pkgs.git}/bin/git pull || echo "No changes to pull."
+        ${pkgs.git}/bin/git fetch --dry-run 2>&1 | grep -q -v 'up to date' && ${pkgs.git}/bin/git pull || echo "Pas de changements à tirer."
       '';
       User = "root";
       Group = "root";
     };
   };
 
+  # Timer pour la mise à jour quotidienne du dépôt Git
   systemd.timers.git-pull = {
-    description = "Periodically pull git repository";
+    description = "Tirage quotidien du dépôt Git";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "hourly";
-      Persistent = true;
+      OnCalendar = "daily";
+      Persistent = true;  # S'exécute après un redémarrage si l'heure prévue est manquée
     };
   };
 }
-#  systemd.services.nix-gc-optimize = {
-#    description = "Nix Garbage Collect and Store Optimization";
-#    serviceConfig = {
-#      Type = "oneshot";
-#      ExecStart = "${pkgs.writeScriptBin "nix-maintenance" ''
-#        #!/bin/sh
-#        ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 30d
-#        ${pkgs.nix}/bin/nix-store --optimize
-#      ''}";
-#      User = "root";
-#      Group = "root";
-#    };
-#    wantedBy = [ "multi-user.target" ];
-#  };
-#
-#  systemd.timers.nix-gc-optimize = {
-#    description = "Weekly timer for Nix garbage collection and optimization";
-##    timerConfig = {
-#      OnCalendar = "Sun 21:30:00";
-#      Persistent = true;
-#    };
-#    wantedBy = [ "timers.target" ];
-#  };
