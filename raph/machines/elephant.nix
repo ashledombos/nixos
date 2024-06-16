cc9161b6570cbdbfee63e010c524c7346311ca75
ashledombos <rj@hodo.fr>
2024-06-16 02:55:20 +0200

    Update elephant.nix

--- raph/machines/elephant.nix
+++ raph/machines/elephant.nix
@@ -9,11 +9,11 @@
     ../tailscale.nix
   ];
 
-#  # Paramètres du noyau nécessaires pour l'hibernation
-#  boot.kernelParams = [
-#    "resume=UUID=<UUID du swap>"
-#    "resume_offset=<Offset du swap>"
-#  ];
+  # Paramètres du noyau nécessaires pour l'hibernation
+  boot.kernelParams = [
+    "resume=UUID=c09eba78-6a7b-4f66-bbc4-0ba9b853bb51"
+    "resume_offset=14956544"
+  ];
 #
 #  # Configuration spécifique à GRUB pour l'hibernation
 #  boot.loader.grub = {
@@ -22,8 +22,7 @@
 #    extraConfig = "resume=UUID=<UUID du swap> resume_offset=<Offset du swap>";
 #  };
 
-  # Assurez-vous que ces valeurs sont correctement remplies avant de reconstruire
-  # Utilisez les commandes suivantes pour obtenir ces informations:
-  # UUID: findmnt -no UUID -T /home/swapfile
-  # Offset: sudo filefrag -e /home/swapfile
+  # Utiliser les commandes suivantes pour obtenir ces informations:
+  # UUID: findmnt -no UUID -T /var/lib/swapfile
+  # Offset: sudo filefrag -e /var/lib/swapfile
 }
