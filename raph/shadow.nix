0270f1c4354b32ebb50b969ba27531eb620eb340
ashledombos <rj@hodo.fr>
2024-06-30 22:08:40 +0200

    Update shadow.nix

--- raph/shadow.nix
+++ raph/shadow.nix
@@ -1,11 +1,26 @@
 { config, pkgs, ... }:
 
 {
-    environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     libva
     libva-utils
     libinput
     xorg.libXau
     xorg.libXdmcp
+    libX11
+    libvdpau-va-gl
+    intel-media-driver
+    vaapiIntel
   ];
+
+  # Configuration des drivers OpenGL et VAAPI
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-media-driver
+    ];
+  };
 }
