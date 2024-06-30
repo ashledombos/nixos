3f97d3951b95648ef434d1d64b321426e41ecd87
ashledombos <rj@hodo.fr>
2024-07-01 00:03:08 +0200

    Update shadow.nix

--- raph/shadow.nix
+++ raph/shadow.nix
@@ -11,6 +11,11 @@
     libvdpau-va-gl
     intel-media-driver
     vaapiIntel
+    kdeApplications.kwallet
+    kdeFrameworks.kwallet
+    kdeFrameworks.kinit
+    kdeFrameworks.kcoreaddons
+    kdeFrameworks.knotifications
   ];
 
   # Configuration des drivers OpenGL et VAAPI
