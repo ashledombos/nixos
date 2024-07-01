588c78bba844eed001d1cca95fc218d3cd5c949e
ashledombos <rj@hodo.fr>
2024-07-01 02:16:42 +0200

    Update shadow.nix

--- raph/shadow.nix
+++ raph/shadow.nix
@@ -14,4 +14,15 @@
 
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ "raphael" ];
+
+  systemd.services.xhost-local-root = {
+    description = "Allow local root access to X server";
+    after = [ "display-manager.service" ];
+    wantedBy = [ "graphical.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.xorg.xhost}/bin/xhost +local:root";
+      RemainAfterExit = true;
+    };
+  };
 }
