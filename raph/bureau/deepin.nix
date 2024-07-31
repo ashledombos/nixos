338891d58a3f9c57dc3839eabfae42739a57dc16
ashledombos <rj@hodo.fr>
2024-08-01 00:38:50 +0200

    Update deepin.nix

--- raph/bureau/deepin.nix
+++ raph/bureau/deepin.nix
@@ -2,27 +2,21 @@
 
 
 {
-
-  # Activation du bureau KDE Plasma.
-  services.displayManager.sddm.enable = true;
+  # Activation du bureau DDE de Deepin
   services.desktopManager.deepin.enable = true;
 
-  security.pam.services.sddm.enableKwallet = true;
-  security.pam.services.login.enableKwallet = true;
-
   # Gestionnaire de connexion SDDM avec activation du pavé numérique
-  services.displayManager.sddm.settings = {
-    General = {
-      GreeterEnvironment = "LANG=fr_FR.UTF-8"; # Langue du greeter
-      Numlock = "on"; # Activation du pavé numérique
+  services.displayManager.sddm = {
+    enable = true;
+    settings = {
+      General = {
+        GreeterEnvironment = "LANG=fr_FR.UTF-8";
+        Numlock = "on";
+      };
     };
   };
 
-  xdg.portal.extraPortals = [
-    pkgs.kdePackages.xdg-desktop-portal-kde
-  ];
-
-  # Paquets KDE Plasma
+  # Paquets supplémentaires
   environment.systemPackages = with pkgs; [
       elegant-sddm
   ];
