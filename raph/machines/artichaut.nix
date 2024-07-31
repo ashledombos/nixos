5615737cd27683a99aee05592ee391fa3e16c3fd
ashledombos <rj@hodo.fr>
2024-07-31 20:43:30 +0200

    Update artichaut.nix

--- raph/machines/artichaut.nix
+++ raph/machines/artichaut.nix
@@ -17,10 +17,6 @@
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.device = "nodev"; # Ou périph de démarrage
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Paquets 
   environment.systemPackages = with pkgs; [
     texliveFull
