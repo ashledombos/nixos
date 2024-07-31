bb8a162d277c60efb47f1976a7bd15c94c590e6e
ashledombos <rj@hodo.fr>
2024-07-31 20:50:06 +0200

    Update and rename raph/plasma.nix to raph/bureau/plasma.nix

---
+++ raph/bureau/plasma.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+
+{
+
+  security.pam.services.sddm.enableKwallet = true;
+  security.pam.services.login.enableKwallet = true;
+
+  # Gestionnaire de connexion SDDM avec activation du pavé numérique
+  services.displayManager.sddm.settings = {
+    General = {
+      GreeterEnvironment = "LANG=fr_FR.UTF-8"; # Langue du greeter
+      Numlock = "on"; # Activation du pavé numérique
+    };
+  };
+
+  xdg.portal.extraPortals = [
+    pkgs.kdePackages.xdg-desktop-portal-kde
+  ];
+
+  # Paquets KDE Plasma
+  environment.systemPackages = with pkgs; [
+    kdePackages.discover
+    skanlite
+  ];
+
+}
