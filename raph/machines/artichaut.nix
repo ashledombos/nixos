062daf0fde83bee41f0ed7898e9a07a4a482b3f3
ashledombos <rj@hodo.fr>
2024-07-31 20:23:23 +0200

    Update artichaut.nix

--- raph/machines/artichaut.nix
+++ raph/machines/artichaut.nix
@@ -10,11 +10,16 @@
     ../vscodium.nix
     # ../portable.nix
   ];
+
   # boot.loader.systemd-boot.enable = false;
   # boot.loader.efi.canTouchEfiVariables = true;
   # boot.loader.grub.enable = true;
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.device = "nodev"; # Ou périph de démarrage
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Paquets 
   environment.systemPackages = with pkgs; [
