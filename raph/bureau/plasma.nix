4fc2a446fbb6dd2b4b758ca126102f7d7d4dc7f8
ashledombos <rj@hodo.fr>
2024-08-01 00:39:36 +0200

    Update plasma.nix

--- raph/bureau/plasma.nix
+++ raph/bureau/plasma.nix
@@ -3,18 +3,20 @@
 
 {
 
-  # Activation du bureau KDE Plasma.
-  services.displayManager.sddm.enable = true;
+  # Activation du bureau KDE Plasma 6.
   services.desktopManager.plasma6.enable = true;
 
   security.pam.services.sddm.enableKwallet = true;
   security.pam.services.login.enableKwallet = true;
 
   # Gestionnaire de connexion SDDM avec activation du pavé numérique
-  services.displayManager.sddm.settings = {
-    General = {
-      GreeterEnvironment = "LANG=fr_FR.UTF-8"; # Langue du greeter
-      Numlock = "on"; # Activation du pavé numérique
+  services.displayManager.sddm = {
+    enable = true;
+    settings = {
+      General = {
+        GreeterEnvironment = "LANG=fr_FR.UTF-8";
+        Numlock = "on";
+      };
     };
   };
 
