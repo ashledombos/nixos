7178354bf0a9176bbf5992f956f55f51016253c9
ashledombos <rj@hodo.fr>
2024-07-31 20:46:01 +0200

    Create modèle_proxy.nix

---
+++ raph/réseau/modèle_proxy.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+  # Configuration proxy si nécessaire
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+}
