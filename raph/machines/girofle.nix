f4d153a976722bdbbc9d47f0a00c500cb09c61f1
ashledombos <rj@hodo.fr>
2024-08-01 10:39:26 +0200

    Update girofle.nix

--- raph/machines/girofle.nix
+++ raph/machines/girofle.nix
@@ -1,1 +1,26 @@
+{ config, pkgs, ... }:
 
+{
+  imports = [
+    ../config.nix
+    ../services.nix
+    ../boot-plymouth.nix
+    ../paquets/tailscale.nix
+    ../paquets/shadow.nix
+    #../paquets/vscodium.nix
+    ./portable.nix
+  ];
+
+  # Configuration de la disposition du clavier par défaut
+   services.xserver = {
+    layout = "fr";
+    xkbVariant = "afnor";
+  };
+
+  networking.hostName = "girofle"; # Definir le nom d’hôte.
+
+  # Paquets 
+  environment.systemPackages = with pkgs; [
+  ];
+
+}
