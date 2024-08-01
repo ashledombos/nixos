29f64da19bcd701de8267ef69b38d6039b52701f
ashledombos <rj@hodo.fr>
2024-08-01 14:51:06 +0200

    Update firefox.nix

--- raph/paquets/firefox.nix
+++ raph/paquets/firefox.nix
@@ -9,57 +9,32 @@
         speechSynthesisSupport = true;
       };
     };
-    
-    profiles = {
-      default = {
-        id = 0;
-        name = "Défaut";
-        isDefault = true;
-        settings = {
-          "intl.locale.requested" = "fr";
-          # "intl.accept_languages" = "fr,fr-FR,en-US,en";
-          
-          # Paramètres de confidentialité
-          # "privacy.donottrackheader.enabled" = true;
-          # "privacy.trackingprotection.enabled" = true;
-          # "privacy.trackingprotection.socialtracking.enabled" = true;
-          
-          # Désactiver Pocket
-          "extensions.pocket.enabled" = false;
-          
-          # Activer HTTPS-Only Mode
-          "dom.security.https_only_mode" = true;
-          "dom.security.https_only_mode_ever_enabled" = true;
-          
-          # Autres paramètres personnalisés
-          # "browser.startup.homepage" = "https://www.mozilla.org/fr/firefox/new/";
-          "browser.search.region" = "FR";
-          "browser.search.isUS" = false;
-        };
-      };
-    };
-    
-    policies = {
-      # DisableTelemetry = true;
-      # DisableFirefoxStudies = true;
-      # DisablePocket = true;
-      # OverrideFirstRunPage = "";
-      # OverridePostUpdatePage = "";
-      # DontCheckDefaultBrowser = true;
-      SearchEngines = {
-        Default = "DuckDuckGo";
-      #  PreventInstalls = true;
-      };
-    };
+  };
+
+  # Configuration de la langue pour Firefox
+  environment.variables = {
+    LANGUAGE = "fr_FR:fr";
   };
 
   # Activer Speech Dispatcher
-  services.speech-dispatcher = {
-    enable = true;
-  };
+  services.speech-dispatcher.enable = true;
 
   # Ajouter Speech Dispatcher aux paquets système
   environment.systemPackages = with pkgs; [
     speech-dispatcher
   ];
+
+  # Configuration globale pour Firefox (optionnel)
+  # environment.etc."firefox/policies/policies.json".text = builtins.toJSON {
+  #   policies = {
+  #    DisableTelemetry = true;
+  #    DisableFirefoxStudies = true;
+  #    DisablePocket = true;
+  #    OverrideFirstRunPage = "";
+  #    OverridePostUpdatePage = "";
+  #    DontCheckDefaultBrowser = true;
+  #    DefaultSearchProviderName = "DuckDuckGo";
+  #    DefaultSearchProviderSearchURL = "https://duckduckgo.com/?q={searchTerms}";
+  #  };
+  #};
 }
