9846fee9da530b76af5e9eeef8aeffe431afe872
ashledombos <rj@hodo.fr>
2024-08-01 14:59:57 +0200

    Update firefox.nix

--- raph/paquets/firefox.nix
+++ raph/paquets/firefox.nix
@@ -15,26 +15,4 @@
   environment.variables = {
     LANGUAGE = "fr_FR:fr";
   };
-
-  # Activer Speech Dispatcher
-  services.speech-dispatcher.enable = true;
-
-  # Ajouter Speech Dispatcher aux paquets système
-  environment.systemPackages = with pkgs; [
-    speech-dispatcher
-  ];
-
-  # Configuration globale pour Firefox (optionnel)
-  # environment.etc."firefox/policies/policies.json".text = builtins.toJSON {
-  #   policies = {
-  #    DisableTelemetry = true;
-  #    DisableFirefoxStudies = true;
-  #    DisablePocket = true;
-  #    OverrideFirstRunPage = "";
-  #    OverridePostUpdatePage = "";
-  #    DontCheckDefaultBrowser = true;
-  #    DefaultSearchProviderName = "DuckDuckGo";
-  #    DefaultSearchProviderSearchURL = "https://duckduckgo.com/?q={searchTerms}";
-  #  };
-  #};
 }
