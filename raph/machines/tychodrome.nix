f2e1e4d741383061e1a0151ed880a26867c9284b
ashledombos <rj@hodo.fr>
2024-08-02 02:11:48 +0200

    Create tychodrome.nix

---
+++ raph/machines/tychodrome.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../commun.nix
+    ../amorce/boot-grub.nix
+    ../bureau/deepin.nix
+    ../paquets/shadow.nix
+    #../paquets/vscodium.nix
+    ../portable.nix
+  ];
+
+  # Configuration de la disposition du clavier par défaut
+   services.xserver.xkb = {
+    layout = "fr";
+    variant = "afnor";
+  };
+
+  networking.hostName = "girofle"; # Definir le nom d’hôte.
+
+  # Paquets 
+  environment.systemPackages = with pkgs; [
+  ];
+
+}
