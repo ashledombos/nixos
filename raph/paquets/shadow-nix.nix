316e87e748573450f42fa7f9725fe7545e9f4699
ashledombos <rj@hodo.fr>
2024-08-03 22:11:37 +0200

    Update shadow-nix.nix

--- raph/paquets/shadow-nix.nix
+++ raph/paquets/shadow-nix.nix
@@ -4,16 +4,15 @@
   # Installer les paquets nécessaires pour ShadowPC
   environment.systemPackages = with pkgs; [
     libva-utils             # Pour le support de VAAPI
-    libvaIntel              # Pour les pilotes Intel
     vaapiVdpau              # Pour le support VDPau
     libvdpau-va-gl          # Pour l'accélération vidéo
     intel-media-driver      # Pilotes pour les médias Intel
-    # gnome-keyring           # Pour la fonctionnalité "Remember Me"
+    gnome-keyring           # Pour la fonctionnalité "Remember Me"
     # Ajoutez d'autres paquets nécessaires ici si besoin
   ];
 
   hardware.opengl = {
     enable = true;  # Activer OpenGL
-    extraPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver ];
+    extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl intel-media-driver ];
   };
 }
