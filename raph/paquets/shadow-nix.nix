bc3e4ca62aff4afb6a5becf00bc91906f1480775
ashledombos <rj@hodo.fr>
2024-08-03 22:25:37 +0200

    Update shadow-nix.nix

--- raph/paquets/shadow-nix.nix
+++ raph/paquets/shadow-nix.nix
@@ -8,15 +8,8 @@
     libvdpau-va-gl          # Pour l'accélération vidéo
     intel-media-driver      # Pilotes pour les médias Intel
     libinput                # Pour la gestion des entrées
-    libX11                  # Pour le support X11
-    libXau                  # Pour libXau
-    libXdmcp                # Pour libXdmcp
-    libva                   # Pour VAAPI
-    libva-glx               # Pour VAAPI avec GLX
-    libva-x11               # Pour VAAPI avec X11
-    libva-wayland           # Pour VAAPI avec Wayland (si nécessaire)
-    libva-drm               # Pour VAAPI avec DRM
-    gnome-keyring           # Pour la fonctionnalité "Remember Me"
+    xorg                    # Inclut libX11 et d'autres bibliothèques X11
+    # gnome-keyring           # Pour la fonctionnalité "Remember Me"
     # Ajoutez d'autres paquets nécessaires ici si besoin
   ];
 
@@ -25,3 +18,4 @@
     extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl intel-media-driver ];
   };
 }
+
