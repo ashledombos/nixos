5e6669d198522a4baf91423ac6df905781f0159d
ashledombos <rj@hodo.fr>
2024-08-05 01:14:09 +0200

    Create etoile.nix

---
+++ raph/machines/etoile.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../commun.nix
+    ../amorce/boot-systemd.nix
+    ../bureau/plasma.nix
+    ../paquets/netbird.nix
+    ../paquets/shadow-nix.nix
+    ../paquets/vscodium.nix
+    ../portable.nix
+  ];
+
+  # Configuration de la disposition du clavier par défaut
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "bepo_afnor";
+  };
+
+  networking.hostName = "etoile"; # Definir le nom d’hôte.
+
+  # Paquets 
+  environment.systemPackages = with pkgs; [
+    veracrypt
+    ecryptfs
+  ];
+
+ # Activer eCryptfs
+  programs.ecryptfs.enable = true;
+
+  # Activez le support eCryptfs pour PAM
+  security.pam.enableEcryptfs = true;
+
+}
