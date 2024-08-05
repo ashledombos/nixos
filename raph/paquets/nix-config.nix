832f1b2148043b511eb740081ac9705d7996eb8f
ashledombos <rj@hodo.fr>
2024-08-05 21:41:01 +0200

    Update nix-config.nix

--- raph/paquets/nix-config.nix
+++ raph/paquets/nix-config.nix
@@ -4,9 +4,9 @@
 
   programs.nix-index = {
   enable = true;
-  # enableBashIntegration = true;
-  # enableZshIntegration = true;
-  # enableFishIntegration = true;
+  enableBashIntegration = true;
+  enableZshIntegration = true;
+  enableFishIntegration = true;
   };
 
   programs.command-not-found.enable = false;
