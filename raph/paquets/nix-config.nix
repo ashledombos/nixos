98168a6f91de6bf3eee792281726955d3c64be8c
ashledombos <rj@hodo.fr>
2024-08-05 21:21:59 +0200

    Create nix-config.nix

---
+++ raph/paquets/nix-config.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  programs.nix-index = {
+  enable = true;
+  # enableBashIntegration = true;
+  # enableZshIntegration = true;
+  # enableFishIntegration = true;
+  };
+
+  programs.command-not-found.enable = false;
+
+}
