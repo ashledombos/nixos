bf807129c713c9ac277ede7a2fee1d5b0372f115
ashledombos <rj@hodo.fr>
2024-08-06 10:30:55 +0200

    Update shadow-nix.nix

--- raph/paquets/shadow-nix.nix
+++ raph/paquets/shadow-nix.nix
@@ -1,16 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  #imports = [
-  #  (import (fetchGit {
-  #    url = "https://github.com/anthonyroussel/shadow-nix";
-  #    ref = "refs/tags/v1.5.0";
-  #  }) + "/import/system.nix")
-  #];
-
-  #programs.shadow-client = {
-  #  channel = "prod";
-  #};
 
     imports = [
     (fetchGit { url = "https://github.com/anthonyroussel/shadow-nix"; ref = "refs/tags/v1.5.0"; } + "/import/system.nix")
@@ -41,24 +31,5 @@
     ];
   };
 
- # environment.systemPackages = with pkgs; [ libva-utils ];
-
-  #nixpkgs.config.packageOverrides = pkgs: {
-  #  vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  #};
-
-  #hardware.opengl = {
-  #  enable = true;
-  #  extraPackages = with pkgs; [
-  #    vaapiIntel
-   #   vaapiVdpau
-  #    libvdpau-va-gl
-  #    intel-media-driver
-  #  ];
-  #};
-
-  #environment.sessionVariables = {
-  #  LIBVA_DRIVER_NAME = "iHD";
-  #};
 }
 
