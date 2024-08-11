17a315ad0b3482a8f154ae34ce32028a8c8ddd5e
ashledombos <rj@hodo.fr>
2024-08-12 00:13:00 +0200

    Update shadow-nix.nix

--- raph/paquets/shadow-nix.nix
+++ raph/paquets/shadow-nix.nix
@@ -3,33 +3,32 @@
 {
 
   imports = [
-    (fetchGit { url = "https://github.com/anthonyroussel/shadow-nix"; ref = "refs/tags/v1.5.0"; } + "/import/system.nix")
+    (fetchGit { url = "https://github.com/cornerman/shadow-nix"; ref = "refs/tags/v2.0.2"; } + "/import/system.nix")
   ];
 
   programs.shadow-client = {
     # Enabled by default when using import
     # enable = true;
-    channel = "prod";
   };
 
   # Provides the `vainfo` command
-  # environment.systemPackages = with pkgs; [ libva-utils ];
+  environment.systemPackages = with pkgs; [ libva-utils ];
 
   # Hardware hybrid decoding
-  #nixpkgs.config.packageOverrides = pkgs: {
-  #  vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  #};
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
 
   # Hardware drivers
-  #hardware.opengl = {
-  #  enable = true;
-  #  extraPackages = with pkgs; [
-  #    vaapiIntel
-  #    vaapiVdpau
-  #    libvdpau-va-gl
-  #    intel-media-driver
-  #  ];
-  #};
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-media-driver
+    ];
+  };
 
 }
 
