51182151b9ec1c3b0c82c394bf43206ee06c5a4c
ashledombos <rj@hodo.fr>
2024-08-12 18:46:30 +0200

    Update shadow-nspawn.nix

--- raph/paquets/shadow-nspawn.nix
+++ raph/paquets/shadow-nspawn.nix
@@ -5,30 +5,19 @@ let
     url = "https://cloud.debian.org/images/cloud/bookworm/20240717-1811/debian-12-generic-amd64-20240717-1811.tar.xz";
     sha256 = "b8111f62baffd1395c5b6640cd6fc2bbaa28f65e4c85f8741659002fdbe862f0";
   };
-
-  setupScript = pkgs.writeScriptBin "setup-debian-container" ''
-    #!${pkgs.stdenv.shell}
-    mkdir -p /var/lib/machines/debian-bookworm
-    tar -xf ${debianImage} -C /var/lib/machines/debian-bookworm
-  '';
-
 in {
+  # Enable the systemd machine targets
   systemd.targets.machines.enable = true;
 
-  systemd.nspawn."debian-bookworm" = {
-    enable = true;
-    directory = "/var/lib/machines/debian-bookworm";  # Spécifier le chemin correct
-    execConfig = { Boot = true; };
-    filesConfig = { BindReadOnly = [ "/etc/resolv.conf:/etc/resolv.conf" ]; };
-    networkConfig = { Private = false; };
-  };
-
+  # Define the service for starting the nspawn container
   systemd.services."systemd-nspawn@debian-bookworm" = {
     enable = true;
     requiredBy = [ "machines.target" ];
     overrideStrategy = "asDropin";
     serviceConfig = {
-      ExecStartPre = "${setupScript}/bin/setup-debian-container"; # S’assurer que le script est exécuté avant le démarrage du service
+      ExecStart = "${pkgs.systemd}/bin/systemd-nspawn --directory=/var/lib/machines/debian-bookworm --boot";
+      BindReadOnlyPaths = [ "/etc/resolv.conf" ];
+      PrivateNetwork = false;
     };
   };
 }
